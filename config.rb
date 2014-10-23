01fcf1a6b20771120888b688c9c188dd089ec303
laurentperroteau <laurentperroteau@gmail.com>
2014-10-23 08:21:48 +0200

    First commit

---
+++ config.rb
@@ -0,0 +1,12 @@
+# ------------------------#
+# ::: COMPASS SETTING ::: #
+# ------------------------#
+
+http_path = "/"
+css_dir = "/"
+sass_dir = "/"
+images_dir = "img/"
+javascripts_dir = "js"
+
+# Enable relative paths to assets via compass helper functions for sprite:
+relative_assets = true
